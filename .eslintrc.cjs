31243337f4954853a961983b2f65684cfa987175
Riceball <42161586+Anderice@users.noreply.github.com>
2023-01-10 08:46:39 +0800

    chore: Add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,21 +2,21 @@ module.exports = {
   // ...
   extends: [
     // ...
-    "plugin:astro/recommended",
-    "plugin:astro/jsx-a11y-recommended",
+    'plugin:astro/recommended',
+    'plugin:astro/jsx-a11y-recommended',
   ],
   // ...
   overrides: [
     {
       // Define the configuration for `.astro` file.
-      files: ["*.astro"],
+      files: ['*.astro'],
       // Allows Astro components to be parsed.
-      parser: "astro-eslint-parser",
+      parser: 'astro-eslint-parser',
       // Parse the script in `.astro` as TypeScript by adding the following configuration.
       // It's the setting you need when using TypeScript.
       parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
       },
       rules: {
         // override/add rules settings here, such as:
