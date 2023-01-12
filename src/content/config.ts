eefc51a4175df0dc8263977f84f18f4574d66b04
Riceball <42161586+Anderice@users.noreply.github.com>
2023-01-13 05:00:46 +0800

    feat: Add post schema

---
+++ src/content/config.ts
@@ -0,0 +1,47 @@
+import { z, defineCollection } from 'astro:content';
+
+function toReadableDate(time: string, seperator: string) {
+  const formattedDate = new Date(time).toLocaleDateString('zh-TW', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
+  return seperator ? formattedDate.replace(/\//g, seperator) : formattedDate;
+}
+
+const post = defineCollection({
+  slug: ({ data, defaultSlug }) => {
+    return data.permalink || defaultSlug;
+  },
+
+  schema: {
+    featureIcon: z
+      .object({
+        width: z.number().default(64),
+        height: z.number().default(64),
+        alt: z.string(),
+        url: z.string(),
+      })
+      .optional(),
+    featureImage: z
+      .object({
+        width: z.number().default(64),
+        height: z.number().default(64),
+        alt: z.string(),
+        url: z.string(),
+      })
+      .optional(),
+    title: z.string(),
+    titleTC: z.string(),
+    excerpt: z.string(),
+    category: z.string().default('unsorted'),
+    tags: z.array(z.string()).default(['unsorted']),
+    themeColor: z.z.string().min(4).max(9).regex(/^#/).default('#fff'),
+    publishDate: z.string().transform((str) => toReadableDate(str, '-')),
+    permalink: z.string().optional(),
+  },
+});
+
+export const collections = {
+  post: post,
+};
