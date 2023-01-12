699c0e76b6b1c3f1543fd44c04437e5896201044
Riceball <42161586+Anderice@users.noreply.github.com>
2023-01-13 04:09:42 +0800

    feat: Add basic info about the site

---
+++ src/config.ts
@@ -0,0 +1,55 @@
+export const brand = {
+  name: 'WebDong',
+  nameTC: '網東',
+  short: 'WD',
+  slogan: '專業的角度有趣的玩網頁',
+  description: '專注於用簡白好懂的方式來傳遞網頁開發與設計相關知識。更快的學習最新出爐的技術，更早的實踐夢想中的專案。',
+  thumbnail: {
+    attributes: {
+      width: 100,
+      height: 100,
+    },
+  },
+};
+
+export const mainNav = [
+  {
+    name: '首頁',
+    url: '/',
+  },
+  {
+    name: '關於',
+    url: '/about',
+  },
+  {
+    name: '文章',
+    url: '/post',
+  },
+];
+
+export const copyright = {
+  title: 'CC BY-NC 4.0',
+  url: 'https://creativecommons.org/licenses/by-nc/4.0/deed.zh_TW',
+};
+export const socials = [
+  {
+    name: 'CodePen',
+    imgUrl: 'https://www.webdong.dev/images/social/codepen.svg',
+    url: 'https://codepen.io/riecball',
+  },
+  {
+    name: 'YouTube',
+    imgUrl: 'https://www.webdong.dev/images/social/youtube.svg',
+    url: 'https://www.youtube.com/channel/',
+  },
+  {
+    name: 'Instagram',
+    imgUrl: 'https://www.webdong.dev/images/social/instagram.svg',
+    url: 'https://www.instagram.com/',
+  },
+  {
+    name: 'GitHub',
+    imgUrl: 'https://www.webdong.dev/images/social/github.svg',
+    url: 'https://github.com/riceball-tw',
+  },
+];
