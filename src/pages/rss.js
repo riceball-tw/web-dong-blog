9aa6ca8d9e63114ef6a821bd9effaa2735fa1bcb
Riceball <42161586+Anderice@users.noreply.github.com>
2023-01-20 04:56:57 +0800

    feat: Add rss

---
+++ src/pages/rss.js
@@ -0,0 +1,19 @@
+import rss from '@astrojs/rss';
+import { brand } from '@/config';
+import { getCollection } from 'astro:content';
+
+export const get = async () => {
+  const posts = await getCollection('post');
+
+  return rss({
+    title: brand.nameTC,
+    description: brand.description,
+    site: import.meta.env.SITE,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.excerpt,
+      pubDate: post.data.publishDate,
+      link: post.slug,
+    })),
+  });
+};
