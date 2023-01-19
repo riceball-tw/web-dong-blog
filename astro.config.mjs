e4dc0d898522e59b3d57376962f19d713e7fd46e
Riceball <42161586+Anderice@users.noreply.github.com>
2023-01-20 03:50:10 +0800

    feat: Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,11 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
 export default defineConfig({
+  site: 'https://www.webdong.dev',
   experimental: {
     contentCollections: true,
   },
@@ -18,5 +22,6 @@ export default defineConfig({
       },
     }),
     mdx(),
+    sitemap(),
   ],
 });
