908a3d74672aa5c10de18592a90feefdfa585ab2
Riceball <42161586+Anderice@users.noreply.github.com>
2023-04-07 15:13:54 +0800

    chore: Edit eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,15 @@
 module.exports = {
-  plugins: ['import'],
-  rules: {
-    // turn on errors for missing imports
-    'import/no-unresolved': 'error',
-  },
+  // ...
+  extends: [
+    // ...
+    'airbnb-base',
+    'plugin:astro/recommended',
+    'plugin:astro/jsx-a11y-recommended',
+    'prettier',
+  ],
+  // ...
   settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
     'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
-
-        // Choose from one of the "project" configs below or omit to use <root>/tsconfig.json by default
-
-        // use <root>/path/to/folder/tsconfig.json
-        project: 'path/to/folder',
-
-        // Multiple tsconfigs (Useful for monorepos)
-
-        // use a glob pattern
-        project: 'packages/*/tsconfig.json',
-
-        // use an array
-        project: ['packages/module-a/tsconfig.json', 'packages/module-b/tsconfig.json'],
-
-        // use an array of glob patterns
-        project: ['packages/*/tsconfig.json', 'other-packages/*/tsconfig.json'],
-      },
       alias: {
         map: [['@', './src']],
         extensions: ['.astro', '.ts', '.tsx', '.js', '.jsx', '.json', '.vue'],
@@ -36,8 +18,12 @@ module.exports = {
   },
   overrides: [
     {
+      // Define the configuration for `.astro` file.
       files: ['*.astro'],
+      // Allows Astro components to be parsed.
       parser: 'astro-eslint-parser',
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
       parserOptions: {
         parser: '@typescript-eslint/parser',
         extraFileExtensions: ['.astro'],
@@ -55,8 +41,7 @@ module.exports = {
         ],
       },
     },
-    {
-      extends: ['airbnb-base', 'plugin:astro/recommended', 'plugin:astro/jsx-a11y-recommended', 'prettier'],
-    },
+
+    // ...
   ],
 };
