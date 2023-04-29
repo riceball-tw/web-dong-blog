51b48da37d0c632e0395a5bda72573912230e229
Riceball <42161586+Anderice@users.noreply.github.com>
2023-04-29 11:05:07 +0800

    feat: Add rehype-external-links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,26 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import partytown from '@astrojs/partytown';
+import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
   site: 'https://www.webdong.dev',
   markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          content: {
+            type: 'element',
+            tagName: 'span',
+            properties: {
+              class: 'external-link-icon',
+            },
+            children: [{ type: 'text', value: '🡕' }],
+          },
+        },
+      ],
+    ],
     shikiConfig: {
       theme: 'monokai',
     },
