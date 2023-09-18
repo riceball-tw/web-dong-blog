86988cebe80fb20966d7b3948aea67da92de2485
Riceball <42161586+Anderice@users.noreply.github.com>
2023-09-18 09:38:07 +0800

    feat: Add search functionality

---
+++ src/pages/search.json.ts
@@ -0,0 +1,21 @@
+import { getCollection } from 'astro:content';
+
+async function getPosts() {
+  const avaliablePosts = await getCollection('post', (post) => (import.meta.env.PROD ? !post.data.isDraft : true));
+  return avaliablePosts.map((post) => ({
+    slug: post.slug,
+    title: post.data.title,
+    titleTC: post.data.titleTC,
+    excerpt: post.data.excerpt,
+    themeColor: post.data.themeColor,
+  }));
+}
+
+export async function GET() {
+  return new Response(JSON.stringify(await getPosts()), {
+    status: 200,
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
+}
