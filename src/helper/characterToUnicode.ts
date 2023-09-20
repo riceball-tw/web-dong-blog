6351c2a9ab5f0bcd6d84ccda1078773fd72167d5
Riceball <42161586+riceball-tw@users.noreply.github.com>
2023-09-20 08:43:05 +0800

    feat: Apply view transition

---
+++ src/helper/characterToUnicode.ts
@@ -0,0 +1,8 @@
+export function characterToUnicode(inputText: string): string {
+  return Array.from(inputText)
+    .map((char) => {
+      const charCode = char.charCodeAt(0).toString(16).toUpperCase();
+      return `\\u${'0'.repeat(4 - charCode.length)}${charCode}`;
+    })
+    .join('');
+}
