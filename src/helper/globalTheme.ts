69c0c078ba2323432b43caa92d858a331fc22b12
Riceball <42161586+riceball-tw@users.noreply.github.com>
2023-10-30 03:48:55 +0800

    refactor: Separating logic for reuse

--- src/helper/globalTheme.ts
+++ src/helper/globalTheme.ts
@@ -4,3 +4,23 @@ export function isGlobalThemeDark() {
   const isLocalStorageDark = localStorage.theme === 'dark';
   return isLocalStorageDark || isSystemPerferDark;
 }
+
+export function getCurrentGlobalTheme() {
+  const defaultTheme = 'light';
+  return localStorage.theme || defaultTheme;
+}
+
+export function toggleGlobalTheme(currentTheme: string) {
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  setGlobalTheme(newTheme, currentTheme);
+}
+
+export function setGlobalTheme(newTheme: string, oldTheme?: string) {
+  console.log('theme', newTheme, oldTheme);
+  localStorage.theme = newTheme;
+  const HTMLElement = document.documentElement;
+  if (oldTheme) {
+    HTMLElement.classList.remove(oldTheme);
+  }
+  HTMLElement.classList.add(newTheme);
+}
