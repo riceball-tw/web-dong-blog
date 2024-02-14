191aeb492b0db6009f3273f730317184e487ff1f
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-02-14 21:02:39 +0800

    feat: Update webmanifest

--- src/pages/manifest.webmanifest.ts
+++ src/pages/manifest.webmanifest.ts
@@ -1,13 +1,19 @@
 import type { APIRoute } from 'astro';
 import globalConfig from '@/globalConfig';
 
+// https://web.dev/articles/add-manifest
 export const GET: APIRoute = () =>
   new Response(
     JSON.stringify({
       name: `${globalConfig.brand.nameTC}`,
       icons: [
-        { src: '/icon-192.png', type: 'image/png', sizes: '192x192' },
-        { src: '/icon-512.png', type: 'image/png', sizes: '512x512' },
+        { src: 'images/brand/favicon/icon-192.png', type: 'image/png', sizes: '192x192' },
+        { src: 'images/brand/favicon/icon-512.png', type: 'image/png', sizes: '512x512' },
+        { src: 'images/brand/favicon/maskable-icon-512.png', type: 'image/png', sizes: '512x512', purpose: 'maskable' },
       ],
+      start_url: '/posts',
+      display: 'fullscreen',
+      theme_color: globalConfig.brand.themeColor,
+      background_color: globalConfig.brand.themeColor,
     }),
   );
