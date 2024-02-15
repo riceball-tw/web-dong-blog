547d70b38bf0f4b6ca7c36f022b3304f5cf997a8
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-02-15 22:14:26 +0800

    feat: Remove unused slug collection property

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,6 @@ import { toolboxSchema, postSchema } from '@/content/schemas.ts';
 
 export const collections = {
   post: defineCollection({
-    slug: ({ data, defaultSlug }) => data.permalink || defaultSlug,
     schema: postSchema,
   }),
   toolbox: defineCollection({
