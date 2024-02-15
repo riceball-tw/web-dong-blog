1392f1fea01e1aa9ff3bbdfc97ef78b2f9afafd6
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-02-15 22:09:18 +0800

    refactor: Make export variable cleaner

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,13 @@
 import { defineCollection } from 'astro:content';
 import { toolboxSchema, postSchema } from '@/content/schemas.ts';
 
-const toolbox = defineCollection({
-  type: 'data',
-  schema: toolboxSchema,
-});
-
-const post = defineCollection({
-  slug: ({ data, defaultSlug }) => data.permalink || defaultSlug,
-  schema: postSchema,
-});
-
 export const collections = {
-  post,
-  toolbox,
+  post: defineCollection({
+    slug: ({ data, defaultSlug }) => data.permalink || defaultSlug,
+    schema: postSchema,
+  }),
+  toolbox: defineCollection({
+    type: 'data',
+    schema: toolboxSchema,
+  }),
 };
