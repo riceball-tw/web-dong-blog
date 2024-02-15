c14bbad4375d92e7c248d66005c7e0a2947fdc65
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-02-15 21:40:48 +0800

    refactor: Extract schemas into  moduel

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,59 +1,15 @@
-import { z, defineCollection } from 'astro:content';
-import uniqolor from 'uniqolor';
+import { defineCollection } from 'astro:content';
+import { toolboxSchema, postSchema } from '@/content/schemas.ts';
 
 const toolbox = defineCollection({
   type: 'data',
-  schema: z.object({
-    featureIcon: z
-      .object({
-        width: z.number().default(64),
-        height: z.number().default(64),
-        alt: z.string(),
-        url: z.string(),
-      })
-      .optional(),
-    titleTC: z.string(),
-    url: z.string(),
-    excerpt: z.string(),
-    category: z.string().default('unsorted'),
-    tags: z.array(z.string()).default(['unsorted']),
-    themeColor: z.string().min(4).max(9).regex(/^#/).default(getDefaultColor),
-  }),
+  schema: toolboxSchema,
 });
 
 const post = defineCollection({
   slug: ({ data, defaultSlug }) => data.permalink || defaultSlug,
-
-  schema: z.object({
-    isDraft: z.boolean().default(true),
-    featureIcon: z.object({
-      width: z.number().default(64),
-      height: z.number().default(64),
-      alt: z.string(),
-      url: z.string(),
-    }),
-    featureImage: z
-      .object({
-        width: z.number().default(64),
-        height: z.number().default(64),
-        alt: z.string(),
-        url: z.string(),
-      })
-      .optional(),
-    title: z.string(),
-    titleTC: z.string(),
-    excerpt: z.string(),
-    category: z.string().default('unsorted'),
-    tags: z.array(z.string()).default(['unsorted']),
-    themeColor: z.string().min(4).max(9).regex(/^#/).default(getDefaultColor),
-    publishDate: z.date(),
-    permalink: z.string().optional(),
-  }),
+  schema: postSchema,
 });
-
-function getDefaultColor() {
-  return uniqolor.random({ saturation: 90, lightness: [70, 90] }).color;
-}
 
 export const collections = {
   post,
---
+++ src/content/schemas.ts
@@ -0,0 +1,49 @@
+import { z } from 'astro:content';
+import uniqolor from 'uniqolor';
+
+function getDefaultColor() {
+  return uniqolor.random({ saturation: 90, lightness: [70, 90] }).color;
+}
+
+export const toolboxSchema = z.object({
+  featureIcon: z
+    .object({
+      width: z.number().default(64),
+      height: z.number().default(64),
+      alt: z.string(),
+      url: z.string(),
+    })
+    .optional(),
+  titleTC: z.string(),
+  url: z.string(),
+  excerpt: z.string(),
+  category: z.string().default('unsorted'),
+  tags: z.array(z.string()).default(['unsorted']),
+  themeColor: z.string().min(4).max(9).regex(/^#/).default(getDefaultColor),
+});
+
+export const postSchema = z.object({
+  isDraft: z.boolean().default(true),
+  featureIcon: z.object({
+    width: z.number().default(64),
+    height: z.number().default(64),
+    alt: z.string(),
+    url: z.string(),
+  }),
+  featureImage: z
+    .object({
+      width: z.number().default(64),
+      height: z.number().default(64),
+      alt: z.string(),
+      url: z.string(),
+    })
+    .optional(),
+  title: z.string(),
+  titleTC: z.string(),
+  excerpt: z.string(),
+  category: z.string().default('unsorted'),
+  tags: z.array(z.string()).default(['unsorted']),
+  themeColor: z.string().min(4).max(9).regex(/^#/).default(getDefaultColor),
+  publishDate: z.date(),
+  permalink: z.string().optional(),
+});
