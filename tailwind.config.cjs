647be2a2bf434db4f7176f0c239dbe53c99d30bb
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-02-18 21:46:50 +0800

    chore: Adjust fluid font line-height

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -128,38 +128,41 @@ module.exports = {
       }),
     },
   },
-  plugins: [require('@tailwindcss/typography'), require('tailwindcss-fluid-type')({
-    // your fluid type settings
-    // works only with unitless numbers
-    // This numbers are the defaults settings
-    settings: {
-      fontSizeMin: 1.125, // 1.125rem === 18px
-      fontSizeMax: 1.25, // 1.25rem === 20px
-      ratioMin: 1.125, // Multiplicator Min
-      ratioMax: 1.2, // Multiplicator Max
-      screenMin: 20, // 20rem === 320px
-      screenMax: 96, // 96rem === 1536px
-      unit: 'rem', // default is rem but it's also possible to use 'px'
-      prefix: '', // set a prefix to use it alongside the default font sizes
-      extendValues: true, // When you set extendValues to true it will extend the default values. Set it to false to overwrite the values.
-    },
-    // Creates the text-xx classes
-    // This are the default settings and analog to the tailwindcss defaults
-    // Each `lineHeight` is set unitless and we think that's the way to go especially in context with fluid type.
-    values: {
-      xs: [-2, 1.6],
-      sm: [-1, 1.6],
-      base: [0, 1.6],
-      lg: [1, 1.6],
-      xl: [2, 1.2],
-      '2xl': [3, 1.2],
-      '3xl': [4, 1.3],
-      '4xl': [5, 1.4],
-      '5xl': [6, 1.4],
-      '6xl': [7, 1.4],
-      '7xl': [8, 1.4],
-      '8xl': [9, 1.2],
-      '9xl': [10, 1.4],
-    },
-  }),],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('tailwindcss-fluid-type')({
+      // your fluid type settings
+      // works only with unitless numbers
+      // This numbers are the defaults settings
+      settings: {
+        fontSizeMin: 1.125, // 1.125rem === 18px
+        fontSizeMax: 1.25, // 1.25rem === 20px
+        ratioMin: 1.125, // Multiplicator Min
+        ratioMax: 1.2, // Multiplicator Max
+        screenMin: 20, // 20rem === 320px
+        screenMax: 96, // 96rem === 1536px
+        unit: 'rem', // default is rem but it's also possible to use 'px'
+        prefix: '', // set a prefix to use it alongside the default font sizes
+        extendValues: true, // When you set extendValues to true it will extend the default values. Set it to false to overwrite the values.
+      },
+      // Creates the text-xx classes
+      // This are the default settings and analog to the tailwindcss defaults
+      // Each `lineHeight` is set unitless and we think that's the way to go especially in context with fluid type.
+      values: {
+        xs: [-2, 1.6],
+        sm: [-1, 1.6],
+        base: [0, 1.6],
+        lg: [1, 1.6],
+        xl: [2, 1.5],
+        '2xl': [3, 1.5],
+        '3xl': [4, 1.5],
+        '4xl': [5, 1.5],
+        '5xl': [6, 1.5],
+        '6xl': [7, 1.5],
+        '7xl': [8, 1.5],
+        '8xl': [9, 1.5],
+        '9xl': [10, 1.5],
+      },
+    }),
+  ],
 };
