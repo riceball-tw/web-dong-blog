08fc25234d70466a20e258f4872bac8acda8308e
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-02-22 20:40:10 +0800

    refactor: Rename variable & Add comment

--- src/helper/globalTheme.ts
+++ src/helper/globalTheme.ts
@@ -1,25 +1,43 @@
-// eslint-disable-next-line import/prefer-default-export
+/**
+ * Checks System preference and LocalStorage if the global theme is set to dark.
+ * @returns whether the global theme is set to dark.
+ */
 export function isGlobalThemeDark() {
   const isSystemPerferDark = !('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches;
   const isLocalStorageDark = localStorage.theme === 'dark';
   return isLocalStorageDark || isSystemPerferDark;
 }
 
-export function getCurrentGlobalTheme() {
-  const defaultTheme = 'light';
-  return localStorage.theme || defaultTheme;
+/**
+ * Returns the current global theme.
+ * If a global theme is provided, it is returned.
+ * Otherwise, the site default theme ('light') is returned.
+ *
+ * @param globalTheme - The global theme to be returned.
+ * @returns The current global theme.
+ */
+export function getCurrentGlobalTheme(globalTheme?: string): string {
+  const siteDefaultTheme = 'light';
+  return globalTheme || siteDefaultTheme;
 }
 
+/**
+ * Toggles the global theme between 'dark' and 'light'.
+ * @param currentTheme - The current theme ('dark' or 'light').
+ */
 export function toggleGlobalTheme(currentTheme: string) {
   const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
   setGlobalTheme(newTheme, currentTheme);
 }
 
+/**
+ * Sets the global theme for the application.
+ * @param newTheme - The new theme to be set.
+ * @param oldTheme - (Optional) The old theme to be removed.
+ */
 export function setGlobalTheme(newTheme: string, oldTheme?: string) {
   localStorage.theme = newTheme;
   const HTMLElement = document.documentElement;
-  if (oldTheme) {
-    HTMLElement.classList.remove(oldTheme);
-  }
+  oldTheme && HTMLElement.classList.remove(oldTheme);
   HTMLElement.classList.add(newTheme);
 }
