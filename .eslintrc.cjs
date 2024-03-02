81e804aa1d865ea7d643ed198e9d8f7638140f7a
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-03-02 11:10:50 +0800

    feat: Design system (#14)
    * chore: Change Linter setting
    
    * refactor: Change tailwind config to esm & TS file
    
    * feat: Add & apply new color - brand, highlight, surface

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,17 @@
 module.exports = {
-  extends: ['airbnb-base', 'plugin:astro/recommended', 'plugin:astro/jsx-a11y-recommended', 'prettier'],
   // ...
-  overrides: [
-    {
-      files: ['*.astro'],
-      parser: 'astro-eslint-parser',
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro'],
-      },
-      rules: {},
-    },
+  globals: {
+    localStorage: true,
+    window: true,
+  },
+  extends: [
+    // ...
+    'airbnb-base',
+    'plugin:astro/recommended',
+    'plugin:astro/jsx-a11y-recommended',
+    'prettier',
   ],
+  // ...
   settings: {
     'import/resolver': {
       alias: {
@@ -20,4 +20,32 @@ module.exports = {
       },
     },
   },
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  overrides: [
+    {
+      files: ['*.ts'],
+      rules: {
+        'import/no-unresolved': [2, { ignore: ['astro:*'] }],
+      },
+    },
+    {
+      // Define the configuration for `.astro` file.
+      files: ['*.astro'],
+      // Allows Astro components to be parsed.
+      parser: 'astro-eslint-parser',
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+      rules: {
+        // ESLint doesn't understand Astro's special import syntax out of the box, To fix this, gnore unresolved imports from 'astro:*'
+        'import/no-unresolved': [2, { ignore: ['astro:*'] }],
+        // Ignore the import/extensions rule for `.astro` files.
+        'import/prefer-default-export': 'off',
+      },
+    },
+  ],
 };
