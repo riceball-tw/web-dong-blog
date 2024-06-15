4ad3d282b5cecb461f78dc3f87a3469807107e5e
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-06-15 13:10:29 +0800

    feat: Add basic post e2e

--- cypress/e2e/post/post.cy.ts
+++ cypress/e2e/post/post.cy.ts
@@ -1,16 +1,31 @@
 describe('Post is effective', () => {
-  it('Page is effective', () => {
-    cy.visit('/post/demo-post', { failOnStatusCode: false });
+  beforeEach(() => {
+    cy.visit('/post/demo-post');
+  });
 
-    // Post Header
+  it('Header should visible and working', () => {
     cy.dataCy('post-icon-container').find('svg').should('exist');
     cy.dataCy('post-title').should('have.prop', 'tagName', 'H1').should('not.be.empty');
     cy.dataCy('post-subtitle').should('not.be.empty');
     cy.dataCy('category').should('not.be.empty');
-    cy.dataCy('tags')
-      .find('[data-cy=tag]')
-      .each(($tag) => {
-        cy.wrap($tag).should('not.be.empty');
+    cy.dataCy('tag-link').each(($link) => {
+      cy.wrap($link).should('not.be.empty');
+      cy.request($link.attr('href')).its('status').should('eq', 200);
+    });
+  });
+
+  it('Comment should load', () => {
+    cy.dataCy('post-comment').scrollIntoView();
+    cy.dataCy('post-comment').should('not.be.empty');
+  });
+
+  it('Heading links should visible and working', () => {
+    cy.dataCy('post-content')
+      .find(
+        'h2 .rehype-auto-link, h3 .rehype-auto-link, h4 .rehype-auto-link, h5 .rehype-auto-link, h6 .rehype-auto-link',
+      )
+      .each(($link) => {
+        cy.request($link.attr('href')).its('status').should('eq', 200);
       });
   });
 });
