9f0f76dc061d0be173c83be7950fa8b5e2703dd5
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-06-18 22:02:38 +0800

    feat: Apply new color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,36 +3,6 @@ import type { PluginUtils } from 'tailwindcss/types/config';
 import defaultTheme from 'tailwindcss/defaultTheme';
 import TailwindShadcnConfig from './tailwind-shadcn-config.ts';
 
-const colors = {
-  inherit: 'inherit',
-  current: 'currentColor',
-  transparent: 'transparent',
-  black: '#000',
-  white: '#fff',
-  debug: 'red',
-  primary: {
-    50: 'rgb(var(--primary-color-50) / <alpha-value>)',
-    100: 'rgb(var(--primary-color-100) / <alpha-value>)',
-    200: 'rgb(var(--primary-color-200) / <alpha-value>)',
-    300: 'rgb(var(--primary-color-300) / <alpha-value>)',
-    400: 'rgb(var(--primary-color-400) / <alpha-value>)',
-    500: 'rgb(var(--primary-color-500) / <alpha-value>)',
-    600: 'rgb(var(--primary-color-600) / <alpha-value>)',
-    700: 'rgb(var(--primary-color-700) / <alpha-value>)',
-    800: 'rgb(var(--primary-color-800) / <alpha-value>)',
-    900: 'rgb(var(--primary-color-900) / <alpha-value>)',
-    1000: 'rgb(var(--primary-color-1000) / <alpha-value>)',
-  },
-  secondaryOld: {
-    light: 'rgb(var(--secondary-color-light) / <alpha-value>)',
-    DEFAULT: 'rgb(var(--secondary-color) / <alpha-value>)',
-  },
-  accent: {
-    light: 'rgb(var(--accent-color-light) / <alpha-value>)',
-    DEFAULT: 'rgb(var(--accent-color) / <alpha-value>)',
-  },
-};
-
 export default {
   presets: [TailwindShadcnConfig],
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './astro.config.mjs'],
@@ -45,21 +15,21 @@ export default {
       sans: ['Noto Sans CJK TC', 'Noto Sans TC', ...defaultTheme.fontFamily.sans],
     },
     colors: {
-      ...colors,
-      surface: {
-        lowest: 'var(--color-surface-lowest)',
-        low: 'var(--color-surface-low)',
-        DEFAULT: 'var(--color-surface-color)',
-        high: 'var(--color-surface-high)',
-        highest: 'var(--color-surface-highest)',
-        inverse: 'var(--color-inverse-surface)',
+      inherit: 'inherit',
+      current: 'currentColor',
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+      debug: 'red',
+      navbar: {
+        DEFAULT: 'hsl(var(--navbar))',
+        foreground: 'hsl(var(--navbar-foreground))',
       },
-      brand: {
-        DEFAULT: 'var(--color-brand)',
-        background: 'var(--color-brand-background)',
+      blockquote: {
+        DEFAULT: 'hsl(var(--blockquote))',
+        foreground: 'hsl(var(--blockquote-foreground))',
       },
-
-      highlight: 'var(--color-highlight)',
+      highlight: 'hsl(var(--highlight))',
     },
     extend: {
       // variable colors with <alpha-value> not working with theme function on plugins
@@ -86,40 +56,23 @@ export default {
               marginBottom: theme('spacing.0'),
             },
             // Light Mode
-            '--tw-prose-body': theme('colors.surface.inverse'),
-            '--tw-prose-headings': theme('colors.surface.inverse'),
-            '--tw-prose-lead': theme('colors.surface.inverse'),
-            '--tw-prose-links': theme('colors.surface.inverse'),
+            '--tw-prose-body': theme('colors.foreground'),
+            '--tw-prose-headings': theme('colors.foreground'),
+            '--tw-prose-lead': theme('colors.foreground'),
+            '--tw-prose-links': theme('colors.foreground'),
             '--tw-prose-bold': theme('colors.highlight'),
-            '--tw-prose-counters': theme('colors.primary[600] / 1'),
-            '--tw-prose-bullets': theme('colors.primary[400] / 1'),
-            '--tw-prose-hr': theme('colors.primary[300] / 1'),
-            '--tw-prose-quotes': theme('colors.surface.inverse'),
-            '--tw-prose-quote-borders': theme('colors.primary[300] / 1'),
-            '--tw-prose-captions': theme('colors.primary[700] / 1'),
-            '--tw-prose-code': theme('colors.primary[900] / 1'),
-            '--tw-prose-pre-code': theme('colors.primary[100] / 1'),
-            '--tw-prose-pre-bg': theme('colors.primary[900] / 1'),
-            '--tw-prose-th-borders': theme('colors.primary[300] / 1'),
-            '--tw-prose-td-borders': theme('colors.primary[200] / 1'),
+            '--tw-prose-counters': theme('colors.foreground'),
+            '--tw-prose-bullets': theme('colors.highlight / 0.4'),
+            '--tw-prose-hr': theme('colors.foreground / 0.4'),
+            '--tw-prose-quotes': theme('colors.foreground'),
+            '--tw-prose-quote-borders': theme('colors.border'),
+            '--tw-prose-captions': theme('colors.foreground'),
+            '--tw-prose-code': theme('colors.foreground'),
+            '--tw-prose-pre-code': theme('colors.foreground'),
+            '--tw-prose-pre-bg': theme('colors.background'),
+            '--tw-prose-th-borders': theme('colors.border'),
+            '--tw-prose-td-borders': theme('colors.border'),
 
-            // Dark Mode
-            '--tw-prose-invert-body': theme('colors.surface.inverse'),
-            '--tw-prose-invert-headings': 'white',
-            '--tw-prose-invert-lead': theme('colors.surface.inverse'),
-            '--tw-prose-invert-links': theme('colors.surface.inverse'),
-            '--tw-prose-invert-bold': theme('colors.highlight'),
-            '--tw-prose-invert-counters': theme('colors.surface.inverse'),
-            '--tw-prose-invert-bullets': theme('colors.primary[600] / 1'),
-            '--tw-prose-invert-hr': theme('colors.surface.inverse'),
-            '--tw-prose-invert-quotes': theme('colors.surface.inverse'),
-            '--tw-prose-invert-quote-borders': theme('colors.surface.inverse'),
-            '--tw-prose-invert-captions': theme('colors.surface.inverse'),
-            '--tw-prose-invert-code': theme('colors.surface.inverse'),
-            '--tw-prose-invert-pre-code': theme('colors.surface.inverse'),
-            '--tw-prose-invert-pre-bg': theme('colors.surface.inverse'),
-            '--tw-prose-invert-th-borders': theme('colors.surface.inverse'),
-            '--tw-prose-invert-td-borders': theme('colors.surface.inverse'),
             'blockquote p:first-of-type::before': true,
             'blockquote p:last-of-type::after': true,
             'code::before': false,
@@ -134,9 +87,9 @@ export default {
               borderRadius: theme('borderRadius.lg'),
               scrollbarWidth: 'thin',
             },
+            // Inline Codeblock
             ':not(pre) > code': {
-              '@apply p-1 bg-primary-200 dark:bg-primary-800 bg-opacity-60 border border-primary-300 dark:border-primary-700 hover:bg-primary-200 hover:border-primary-400 dark:hover:bg-primary-700 dark:hover:border-primary-600 transition-colors':
-                {},
+              '@apply p-1 bg-primary text-foreground bg-opacity-20 border border-primary border-opacity-40': {},
             },
           },
         },
