605c9fbe9402e49c01167ce785d1db66d0f5d2e6
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-06-18 22:07:56 +0800

    fix: Skip external code linting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
+// External code, skip linting
+// eslint-disable-next-line import/prefer-default-export
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
