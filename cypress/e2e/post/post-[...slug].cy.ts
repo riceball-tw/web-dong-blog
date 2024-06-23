3216da4922e53b38953fbdbc1c9e8ce4acdc4bcc
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-06-23 22:16:01 +0800

    feat: Better table of content (#81)
    * refactor: Remove custom css, switch to tailwind
    
    * feat: Add active link style transition
    
    * feat: Better scroll margin top, more space between heading and nav
    
    * feat: Rewrite toc style & add manual active
    
    * feat: Rewrite post table of content
    
    * feat: Apply accordion to toc
    
    * fix: Post sidebar overflow
    
    * feat: Make long heading example in demo-post
    
    * fix: Linter issue
    
    * test: Check toc links is correct, check click toc will direct to the related heading
    
    * test: Page Scroll seems not working at the time, so I just force click the link outside of the viewport for now
    
    * feat: Add browser color scheme for better accordion scroll style
    
    * feat: Better scrollbar style for small page component
    
    * feat: Better toc spacing style

--- cypress/e2e/post/post-[...slug].cy.ts
+++ cypress/e2e/post/post-[...slug].cy.ts
@@ -29,6 +29,38 @@ describe('Post is effective', () => {
       });
   });
 
+  it('Table of contents should visible and working', () => {
+    cy.viewport('macbook-15');
+
+    // Check all post title is available in TOC
+    cy.dataCy('toc-link').each(($link) => {
+      cy.wrap($link)
+        .invoke('text')
+        .then((text) => {
+          cy.wrap(text).should('not.be.empty');
+          cy.dataCy('post-content').contains(text);
+        });
+      cy.request('GET', $link.attr('href')).its('status').should('eq', 200);
+    });
+
+    // Check when click last TOC link, will direct to the related heading
+
+    // cy.dataCy('toc').within(() => {
+    // cy.dataCy('accordion-content').scrollTo('bottom');
+    // });
+    // cy.window().scrollTo('bottom'); // Seems like scroll needs time?
+    // No idea why next action will cause scrolling to bottom stop
+    // So I added this line to avoid next action stop the scrolling action too early
+    // cy.window().its('scrollY').should('not.equal', 0);
+
+    cy.dataCy('toc-link')
+      .last()
+      .then(($lastLink) => {
+        cy.wrap($lastLink).click({ force: true }); // Page Scroll seems not working at the time, so I just force click the link outside of the viewport for now :(
+        cy.url().should('include', $lastLink.attr('href'));
+      });
+  });
+
   it('Series recommendation should visible and working', () => {
     cy.dataCy('series-summary').should('not.be.empty');
     cy.dataCy('post-title')
