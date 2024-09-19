beca46dd7ab98069f5ab8ef2713543475f4b8c8a
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-09-19 22:22:21 +0800

    feat: Add post comment skeleton (#118)
    * feat: Add shadcn skeleton
    
    * feat: Add post skeleton
    
    * fix: ESlint error

---
+++ src/components/atoms/skeleton/Skeleton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+import { cn } from '@/utils/shadcn';
+
+interface SkeletonProps {
+  class?: HTMLAttributes['class'];
+  style?: HTMLAttributes['style'];
+}
+
+const props = defineProps<SkeletonProps>();
+</script>
+
+<template>
+  <div
+    :style="props.style"
+    :class="
+      cn('animate-pulse rounded-md border border-foreground/30 bg-background/80 dark:border-foreground/40', props.class)
+    "
+  >
+    <slot />
+  </div>
+</template>
