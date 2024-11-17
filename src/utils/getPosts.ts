376ec035c332eefcac047c2041ea614b93475714
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-11-17 16:44:27 +0800

    feat: Add post i18n contents

--- src/utils/getPosts.ts
+++ src/utils/getPosts.ts
@@ -1,4 +1,6 @@
 import { getCollection } from 'astro:content';
+import { type LanguageKey } from '@/utils/i18n.ts';
+import { getLocaleCode } from '@/utils/i18n.ts';
 
 const isProductionEnvironment = import.meta.env.MODE === 'production';
 
@@ -11,6 +13,11 @@ export const publishedPosts = await getCollection('post', (post) => {
 export const dateSortedPublishedPosts = publishedPosts.sort(
   (a, b) => b.data.publishDate.getTime() - a.data.publishDate.getTime(),
 );
+
+export const dateSortedLocaleRelatedPosts = (currentLocale: LanguageKey) =>
+  publishedPosts
+    .sort((a, b) => b.data.publishDate.getTime() - a.data.publishDate.getTime())
+    .filter((post) => getLocaleCode(post.slug) === currentLocale);
 
 export const sortedPostCategoryies = [...new Set(publishedPosts.map((post) => post.data.category).flat())].sort(
   (a, b) => a.localeCompare(b, 'en', { sensitivity: 'base' }),
