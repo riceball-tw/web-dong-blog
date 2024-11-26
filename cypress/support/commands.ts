edcc7d6f23367a5ed7b4ad4473e588cf52f8be6e
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-11-26 16:42:22 +0800

    fix: Broken-homepage-post-links & taxonomy content (#150)
    * fix: Remove locale in slug cause broken link
    
    * feat: Avoid show non-avaliable category in homepage
    
    * fix: Avoid taxonomy pages show irrelevant locale content
    
    * test: Fix broken change lang button test
    
    * chore: Update lighthouse setting

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,13 +1,1 @@
 Cypress.Commands.add('dataCy', (value) => cy.get(`[data-cy=${value}]`));
-
-// Can you test if an element is scrolled to
-// https://stackoverflow.com/a/70145908/14927277
-Cypress.Commands.add('isScrolledTo', { prevSubject: true }, (element) => {
-  cy.get(element).should(($el) => {
-    const windowHeight = Cypress.$(cy.state('window')).height();
-    const bottom = typeof windowHeight === 'number' ? windowHeight : 0;
-    const rect = $el[0].getBoundingClientRect();
-    expect(rect.top).not.to.be.greaterThan(bottom, `Expected element not to be below the visible scrolled area`);
-    expect(rect.top).to.be.greaterThan(0 - rect.height, `Expected element not to be above the visible scrolled area`);
-  });
-});
