72c82781ead2b8cb38ea7cf17577f64ba3d9c49f
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-12-28 17:09:06 +0800

    fix: Avoid unused type in env.d.ts cause CI crash

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
+// eslint-disable-next-line no-unused-vars
 interface Window {
   theme: {
+    // eslint-disable-next-line no-unused-vars
     setTheme: (theme: 'auto' | 'dark' | 'light') => void;
     getTheme: () => 'auto' | 'dark' | 'light';
     getSystemTheme: () => 'light' | 'dark';
