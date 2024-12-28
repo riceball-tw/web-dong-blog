7678dded40ef8439f6ab937368b2ff43faa51e0f
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-12-28 21:20:29 +0800

    feat: Rewrite theme mode (#158)
    * feat: Remove theme functionality
    
    * feat: Remake theme system
    
    * fix: Avoid unused type in env.d.ts cause CI crash
    
    * test: Make darkmode e2e test work, new selector & naming

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+// eslint-disable-next-line no-unused-vars
+interface Window {
+  theme: {
+    // eslint-disable-next-line no-unused-vars
+    setTheme: (theme: 'auto' | 'dark' | 'light') => void;
+    getTheme: () => 'auto' | 'dark' | 'light';
+    getSystemTheme: () => 'light' | 'dark';
+    getDefaultTheme: () => 'auto' | 'dark' | 'light';
+  };
+}
