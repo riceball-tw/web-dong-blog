e7f8ac9153c3fd85179d0cf0328bcfd84624b3e0
riceball-tw <42161586+riceball-tw@users.noreply.github.com>
2025-03-24 16:53:51 +0800

    refactor: Separate layouts type to new folder for better organization

---
+++ src/types/layouts.ts
@@ -0,0 +1,16 @@
+export interface GlobalHead {
+  title?: string;
+  description?: string;
+  thumbnail?: {
+    width?: number;
+    height?: number;
+    src: string;
+    alt: string;
+  };
+}
+
+export interface ThemeColor {
+  themeColor?: string | undefined;
+}
+
+export interface BaseLayout extends GlobalHead, ThemeColor {}
--- src/types/types.ts
+++ src/types/types.ts
@@ -2,23 +2,6 @@ import type { InferGetStaticPropsType } from 'astro';
 import { getStaticPaths as PostGetStaticPaths } from '@/pages/post/[...slug].astro';
 import { getStaticPaths as ShortpostGetStaticPaths } from '@/pages/shortpost/[...slug].astro';
 
-export interface GlobalHead {
-  title?: string;
-  description?: string;
-  thumbnail?: {
-    width?: number;
-    height?: number;
-    src: string;
-    alt: string;
-  };
-}
-
-export interface themeColor {
-  themeColor?: string | undefined;
-}
-
-export interface BaseLayout extends GlobalHead, themeColor {}
-
 export interface CategoryList {
   categoryHref?: string;
   category?: string;
