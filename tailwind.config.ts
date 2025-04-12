d29f8dcd1efc869ecc1173522ae82840f0f0771f
riceball-tw <42161586+riceball-tw@users.noreply.github.com>
2025-04-12 15:54:34 +0800

    chore: Update tailwind related packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,43 +1,15 @@
+// tailwind.config.ts
 import type { Config } from 'tailwindcss';
-import type { PluginUtils } from 'tailwindcss/types/config';
-import defaultTheme from 'tailwindcss/defaultTheme';
-import TailwindShadcnConfig from './tailwind-shadcn-config.ts';
+// @ts-expect-error
+// tailwindcss-fluid-type has no type, so ignore ts-error
+import fluidType from 'tailwindcss-fluid-type';
 
 export default {
-  presets: [TailwindShadcnConfig],
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './astro.config.mjs'],
-  darkMode: ['selector', '[data-theme="dark"]'],
-  important: true,
   theme: {
-    fontFamily: {
-      mono: ['JetBrains Mono', 'Fira Code', ...defaultTheme.fontFamily.mono],
-      serif: [...defaultTheme.fontFamily.serif],
-      sans: ['Noto Sans CJK TC', 'Noto Sans TC', ...defaultTheme.fontFamily.sans],
-    },
-    colors: {
-      inherit: 'inherit',
-      current: 'currentColor',
-      transparent: 'transparent',
-      black: '#000',
-      white: '#fff',
-      debug: 'red',
-      navbar: {
-        DEFAULT: 'hsl(var(--navbar))',
-        foreground: 'hsl(var(--navbar-foreground))',
-      },
-      blockquote: {
-        DEFAULT: 'hsl(var(--blockquote))',
-        foreground: 'hsl(var(--blockquote-foreground))',
-      },
-      'primary-active': {
-        DEFAULT: 'hsl(var(--primary-active))',
-      },
-      highlight: 'hsl(var(--highlight))',
-    },
     extend: {
       // variable colors with <alpha-value> not working with theme function on plugins
       // https://github.com/tailwindlabs/tailwindcss/issues/9143#issuecomment-1579382345
-      typography: ({ theme }: PluginUtils) => ({
+      typography: ({ theme }) => ({
         '2xl': {
           css: {
             img: {
@@ -92,8 +64,11 @@ export default {
               marginTop: 0,
             },
             kbd: {
-              '@apply p-1 bg-primary text-foreground bg-opacity-20 border border-primary border-opacity-40 shadow-none':
-                {},
+              padding: theme('spacing.1'),
+              background: 'oklch(0.639 0.258 287.2 / 20%);',
+              border: '1px solid oklch(0.639 0.258 287.2 / 40%)',
+              color: theme('colors.foreground'),
+              // '@apply p-1 bg-primary text-foreground bg-opacity-20 border border-primary border-opacity-40 shadow-none': {},
             },
             video: {
               borderRadius: theme('borderRadius.lg'),
@@ -104,7 +79,10 @@ export default {
             },
             // Inline Codeblock
             ':not(pre) > code': {
-              '@apply p-1 bg-primary text-foreground bg-opacity-20 border border-primary border-opacity-40': {},
+              padding: theme('spacing.1'),
+              background: 'oklch(0.639 0.258 287.2 / 20%);',
+              border: '1px solid oklch(0.639 0.258 287.2 / 40%)',
+              // '@apply p-1 bg-primary text-foreground bg-opacity-20 border border-primary border-opacity-40': {},
             },
           },
         },
@@ -112,39 +90,36 @@ export default {
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('tailwindcss-fluid-type')({
-      // your fluid type settings
-      // works only with unitless numbers
-      // This numbers are the defaults settings
-      settings: {
-        fontSizeMin: 1.125, // 1.125rem === 18px
-        fontSizeMax: 1.25, // 1.25rem === 20px
-        ratioMin: 1.125, // Multiplicator Min
-        ratioMax: 1.2, // Multiplicator Max
-        screenMin: 20, // 20rem === 320px
-        screenMax: 96, // 96rem === 1536px
-        unit: 'rem', // default is rem but it's also possible to use 'px'
-        prefix: '', // set a prefix to use it alongside the default font sizes
-        extendValues: true, // When you set extendValues to true it will extend the default values. Set it to false to overwrite the values.
+    fluidType({
+      corePlugins: {
+        fontsize: false,
+        lineHeight: false,
       },
-      // Creates the text-xx classes
-      // This are the default settings and analog to the tailwindcss defaults
-      // Each `lineHeight` is set unitless and we think that's the way to go especially in context with fluid type.
+      settings: {
+        fontSizeMin: 1.125,
+        fontSizeMax: 1.25,
+        ratioMin: 1.125,
+        ratioMax: 1.2,
+        screenMin: 20,
+        screenMax: 96,
+        unit: 'rem',
+        prefix: '',
+        extendValues: true,
+      },
       values: {
-        xs: [-2, 1.6],
-        sm: [-1, 1.6],
-        base: [0, 1.6],
-        lg: [1, 1.6],
-        xl: [2, 1.5],
-        '2xl': [3, 1.5],
-        '3xl': [4, 1.5],
-        '4xl': [5, 1.4],
-        '5xl': [6, 1.4],
-        '6xl': [7, 1.3],
-        '7xl': [8, 1.3],
-        '8xl': [9, 1.2],
-        '9xl': [10, 1.2],
+        xs: -2,
+        sm: -1,
+        base: 0,
+        lg: 1,
+        xl: 2,
+        '2xl': 3,
+        '3xl': 4,
+        '4xl': 5,
+        '5xl': 6,
+        '6xl': 7,
+        '7xl': 8,
+        '8xl': 9,
+        '9xl': 10,
       },
     }),
   ],
