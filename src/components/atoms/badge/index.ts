6f6fd2d1ba3ed108e3aa8780eb2459f5d4cfe4da
riceball <42161586+riceball-tw@users.noreply.github.com>
2025-10-10 16:17:52 +0800

    feat: Page style tweak

--- src/components/atoms/badge/index.ts
+++ src/components/atoms/badge/index.ts
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 export { default as Badge } from "./Badge.vue";
 
 export const badgeVariants = cva(
-	"inline-flex items-center rounded-full border px-4 py-2 text-xs  tracking-wider transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+	"inline-flex items-center rounded-full border px-4 py-2 text-xs  tracking-wider transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-xs",
 	{
 		variants: {
 			variant: {
