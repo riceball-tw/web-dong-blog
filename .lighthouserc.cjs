99244442a31aaf8a0dc35b2bce8765f6ae520a40
riceball <42161586+riceball-tw@users.noreply.github.com>
2025-11-01 21:59:21 +0800

    fix: Linter error

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -30,7 +30,7 @@ module.exports = {
 			},
 		},
 		upload: {
-			target: 'temporary-public-storage',
+			target: "temporary-public-storage",
 			// target: "lhci",
 			// serverBaseUrl: process.env.PUBLIC_LHCI_BASE_URL,
 			// token: process.env.LHCI_TOKEN,
