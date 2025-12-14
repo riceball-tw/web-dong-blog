1930537258026f3ccc136f5170e43775d2103eb0
riceball-tw <42161586+riceball-tw@users.noreply.github.com>
2025-12-14 19:30:27 +0800

    chore(robots.txt.ts): Ignore cdn-cgi

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,8 +1,11 @@
 import type { APIRoute } from "astro";
 
+// cdn-cgi
+// https://developers.cloudflare.com/fundamentals/reference/cdn-cgi-endpoint/#disallow-using-robotstxt
 const robotsTxt = `
 User-agent: *
 Allow: /
+Disallow: /cdn-cgi/
 
 Sitemap: ${new URL("sitemap-index.xml", import.meta.env.SITE).href}
 `.trim();
