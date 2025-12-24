910207e5bd9f05e404d8b9b79c4b6ce4e67d3d0a
riceball <42161586+riceball-tw@users.noreply.github.com>
2025-12-24 22:05:52 +0800

    feat(og): Add cache

--- functions/api/og/[[route]].ts
+++ functions/api/og/[[route]].ts
@@ -1,26 +1,20 @@
 // og-image-generator-cloudflare-worker
 // https://github.com/mohdlatif/og-image-generator-cloudflare-worker/tree/main
 import { Hono } from "hono";
+import { cache } from "hono/cache";
 import { handle } from "hono/cloudflare-pages";
-// import { logger } from 'hono/logger';
-// import { cache } from 'hono/cache';
 import og from "./og";
 
 const app = new Hono().basePath("/api/og");
 
-app
-	// .use('*', logger())
-	// .use(
-	//   '*',
-	//   cache({
-	//     cacheName: async (c) => {
-	//       const url = new URL(c.req.url);
-	//       const params = url.searchParams.toString();
-	//       return `${c.req.method} ${url.pathname}${params}`;
-	//     },
-	//     cacheControl: 'max-age=86400', // 24 hours
-	//   })
-	// )
-	.route("/", og);
+app.use(
+	"*",
+	cache({
+		cacheName: "og-image",
+		cacheControl: "max-age=86400", // 24 hours
+	}),
+);
+
+app.route("/", og);
 
 export const onRequest = handle(app);
